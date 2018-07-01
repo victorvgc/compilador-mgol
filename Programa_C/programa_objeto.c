6e43c32d51aa216192a3c1b66c0a7aa1423af77e
Victor Carvalho <victorvgc@hotmail.com>
2018-06-30 23:52:59 -0300

    Adicionado caminho para a pasta do programa objeto.

---
+++ Programa_C/programa_objeto.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+typedef char lit[256];
+void main(void) {
+/*----Variaveis Temporarias----*/
+int t1;
+int t2;
+int t3;
+int t4;
+int t5;
+int t6;
+/*-----------------------------*/
+int A;
+int B;
+int D;
+double C;
+lit testeID;
+printf("Digite B")
+scanf("%d", &B);
+printf("Digite A:")
+scanf("%d", &A);
+t1 = B>2
+if (t1) {t2 = B<=4
+if (t2) {printf("B esta entre 2 e 4")
+}}t3 = B + 1
+B = t3;
+t4 = B + 2
+B = t4;
+t5 = B + 3
+B = t5;
+D = B;
+C = 5.0;
+t6 = 10 * 2
+A = t6;
+printf("\nB=\n")
+printf(D)
+printf("\n")
+printf(C)
+printf("\n")
+printf(A)
+scanf("%s", testeID);
+printf(testeID)
+}
